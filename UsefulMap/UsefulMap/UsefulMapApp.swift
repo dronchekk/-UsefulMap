83c3c49e6e94f2938f06dec6171f3c1fb4527033
Andrey rachitsky <>
2022-11-20 13:42:52 +0300

    initial commit

---
+++ UsefulMap/UsefulMap/UsefulMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulMapApp.swift
+//  UsefulMap
+//
+//  Created by Andrey Rachitskiy on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct UsefulMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
